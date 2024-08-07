a501c6da0276e5501b672d0cc6c3f8c7262c2a0d
Alex Seeley <alexseeley134@gmail.com>
2024-08-07 11:29:50 -0600

    Add hash function

--- hash-map.js
+++ hash-map.js
@@ -1,1 +1,12 @@
-console.log("HIYA");
+function hash(key) {
+  let hashCode = 0;
+  let prime = 31;
+
+  for (let i = 0; i < key.length; i++) {
+    hashCode = prime * hashCode + key.charCodeAt(i);
+  }
+
+  return hashCode;
+}
+
+console.log(hash("pete"));
